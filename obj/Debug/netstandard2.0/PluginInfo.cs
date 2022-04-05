ff319e8ae420a3f11e1652ba6d7fe8d74bdb77a1
DeepDarkDweller <cnc33@students.uwf.edu>
2022-04-05 16:33:33 -0500

    Include Plugin Info and the build dll

---
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ROR2TestMod
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "ItemCheatTestMod";
+        public const string PLUGIN_NAME = "ItemCheatTestMod";
+        public const string PLUGIN_VERSION = "1.0.1";
+    }
+}
